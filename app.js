5e51e82db4c1e39fbcd08575400c0822123a8e8d
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-04-03 19:26:22 -1000

    With comments

--- app.js
+++ app.js
@@ -4,26 +4,29 @@ module.exports = ()=>{
 
   function bubbleSort(arr){
 
+// Will check if the data entered is an array
     dataType(arr);
 
     var temp;
-    var swap = true;
 
+// Core of the algorithm that will loop to sort
     for (let i=0; i < arr.length-1; i++){
-      for (let j=0; j < arr.length-1-i; j++){
+// The second loop will decrease in length with i as after each iteration of i, the greatest number will be at the end of the array
+      for (let j=0; j < arr.length - (1+i) ; j++){
         if(arr[j] > arr[j+1]){
           temp = arr[j];
           arr[j] = arr[j+1];
-          arr[j+1]= temp;
+          arr[j+1] = temp;
         }
       }
     }
-
+// Return the array
     return arr;
   }
 
+//function for the data type
   function dataType(arr){
-    if(!(arr instanceof Array)){
+    if(!(Array.isArray(arr))){
       throw new Error("Please pass an array");
     }
   }
